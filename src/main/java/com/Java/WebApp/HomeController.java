590cc22818381cb8fe803971c541f7130a1ec51e
vulture02 <amithp0210@gmail.com>
2025-06-25 16:09:26 +0530

    first commit

---
+++ src/main/java/com/Java/WebApp/HomeController.java
@@ -0,0 +1,19 @@
+package com.Java.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String greet(){
+        return "hello Amith";
+    }
+    @RequestMapping("/about")
+    public String about(){
+        return "hi welcome to shop";
+    }
+
+}
